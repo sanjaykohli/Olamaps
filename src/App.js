0e59e311a6eb1a7d8a4939087cf3380d18f57f45
JayArcher9 <cybernetofficial2412@gmail.com>
2024-08-08 14:15:44 +0530

    Upload desktop

---
+++ src/App.js
@@ -0,0 +1,54 @@
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import { useState } from "react";
+import DeckGL from "@deck.gl/react";
+import StaticMap from "react-map-gl";
+import maplibregl from "maplibre-gl";
+
+import "maplibre-gl/dist/maplibre-gl.css";
+
+export default function App() {
+  const [viewState, setViewState] = useState({
+    longitude: 0,
+    latitude: 0,
+    zoom: 1,
+  });
+
+  return (
+    <div>
+      <DeckGL
+        style={{ width: "100vw", height: "100vh", overflow: "hidden" }}
+        viewState={viewState}
+        onViewStateChange={({ viewState }) => setViewState(viewState)}
+        controller={true}
+        layers={[]}
+      >
+        <StaticMap
+          mapLib={maplibregl}
+          mapStyle="https://api.olamaps.io/tiles/vector/v1/styles/default-light-standard/style.json"
+          transformRequest={(url, resourceType) => {
+            if(!url.includes('?')){
+              url = url + '?api_key=rza4jnvA7TORC0kAnHIsliN3HJUlXi0spoqsytwU';
+            }
+            else{
+              url = url + '&api_key=rza4jnvA7TORC0kAnHIsliN3HJUlXi0spoqsytwU';
+
+            }
+            return { url, resourceType };
+          }}
+        />
+      </DeckGL>
+    </div>
+  );
+    };
+
+    const rootElement = document.getElementById("root");
+    const root = createRoot(rootElement);
+
+    root.render(
+      <StrictMode>
+        <App />
+      </StrictMode>
+    );
